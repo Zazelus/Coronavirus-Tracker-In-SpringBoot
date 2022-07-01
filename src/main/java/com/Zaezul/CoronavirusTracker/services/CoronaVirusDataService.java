f00e11f3f219e5f50717cb6ebb16140fea94ff96
Zazelus <mansour091800@gmail.com>
2022-07-01 14:23:50 -0400

    Set up basic http request and response in the VirusDataService

--- src/main/java/com/Zaezul/CoronavirusTracker/services/CoronaVirusDataService.java
+++ src/main/java/com/Zaezul/CoronavirusTracker/services/CoronaVirusDataService.java
@@ -1,16 +1,25 @@
 package com.Zaezul.CoronavirusTracker.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+@Service
 public class CoronaVirusDataService {
 
     private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/06-30-2022.csv";
-
-    public void fetchData() {
+    @PostConstruct
+    public void fetchData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(DATA_URL)).build();
+
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
     }
 
 }
