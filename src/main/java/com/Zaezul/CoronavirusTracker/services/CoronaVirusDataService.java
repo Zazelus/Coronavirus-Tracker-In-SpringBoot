ada2710e1dea5a3c7d4ff7f389f7f32d627de610
Zazelus <mansour091800@gmail.com>
2022-07-01 14:39:07 -0400

    Added CSV parser

--- src/main/java/com/Zaezul/CoronavirusTracker/services/CoronaVirusDataService.java
+++ src/main/java/com/Zaezul/CoronavirusTracker/services/CoronaVirusDataService.java
@@ -1,9 +1,12 @@
 package com.Zaezul.CoronavirusTracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -19,7 +22,14 @@ public class CoronaVirusDataService {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(DATA_URL)).build();
 
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        for (CSVRecord record : records) {
+            String state = record.get("Province_State");
+            System.out.println(state);
+        }
     }
 
 }
