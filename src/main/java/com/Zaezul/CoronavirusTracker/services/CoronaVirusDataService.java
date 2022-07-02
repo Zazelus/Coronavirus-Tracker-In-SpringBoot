6a07583047f202c71892fb780bb1e0dbeaf2e103
Zazelus <mansour091800@gmail.com>
2022-07-02 09:36:57 -0400

    Added scheduling to the data service and a way to fetch the most current csv through String transformation of the URL

--- src/main/java/com/Zaezul/CoronavirusTracker/services/CoronaVirusDataService.java
+++ src/main/java/com/Zaezul/CoronavirusTracker/services/CoronaVirusDataService.java
@@ -2,6 +2,7 @@ package com.Zaezul.CoronavirusTracker.services;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,13 +12,47 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
+/**
+ * This class is responsible from fetching raw csv data and parsing it into a readable format.
+ * It will get updated data from the CSSEGISandData repository on schedule.
+ */
 @Service
 public class CoronaVirusDataService {
 
-    private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/06-30-2022.csv";
+    // Grabbing the previous day as that will have the most recent data.
+    private static Date currentDate = new Date();
+    private static LocalDate localDate = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    private static String currentYear  = String.valueOf(localDate.getYear());
+    private static String currentMonth = String.valueOf(localDate.getMonthValue());
+    private static String currentDay   = String.valueOf(localDate.getDayOfMonth() - 1);
+    private static String DATA_URL;
+
+    /**
+     * File names follow a format like: MM-DD-YYYY, so we'll need to format the current month and days if they are
+     * greater than 10.
+     */
+    public void formatDateAndCreateURL() {
+        if (Integer.parseInt(currentMonth) < 10) {currentMonth = "0" + currentMonth;}
+        if (Integer.parseInt(currentDay) < 10) {currentDay = "0" + currentDay;}
+
+        DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/" +
+                "csse_covid_19_daily_reports/" + currentMonth + "-" + currentDay + "-" + currentYear + ".csv";
+    }
+
+    /**
+     * Scheduled method to be run that pulls data from latest csv as an HttpResponse. Using apache's csv library, we
+     * can parse through each record with the first containing all of our headers.
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @PostConstruct
+    @Scheduled(cron = "* * * * * *")
     public void fetchData() throws IOException, InterruptedException {
+        formatDateAndCreateURL();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(DATA_URL)).build();
 
