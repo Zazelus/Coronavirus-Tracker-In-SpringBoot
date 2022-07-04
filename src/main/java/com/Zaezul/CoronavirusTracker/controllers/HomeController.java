0e03120f2f42f79639249063e35fb93151073fdb
Zazelus <mansour091800@gmail.com>
2022-07-04 09:48:10 -0400

    Made the web page look a little nicer.

--- src/main/java/com/Zaezul/CoronavirusTracker/controllers/HomeController.java
+++ src/main/java/com/Zaezul/CoronavirusTracker/controllers/HomeController.java
@@ -18,7 +18,11 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalCases = allStats.stream().mapToInt(stat -> stat.getTotalCases()).sum();
+
         model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalCases);
+
         return "index";
     }
 }
